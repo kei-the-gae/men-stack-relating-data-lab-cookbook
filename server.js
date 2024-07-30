ceed38dec6775f578afbfe637a4ffae5bd420ae1
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-30 14:47:42 -0700

    Install packages, add .env and .gitignore, customize index page for new app and clean up server

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT ? process.env.PORT : '3000';
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+  console.log(`Connected to MongoDB ${mongoose.connection.name}`);
 });
 
 app.use(express.urlencoded({ extended: false }));
@@ -29,21 +29,11 @@ app.use(
 );
 
 app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
-});
-
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
+  res.render('index.ejs');
 });
 
 app.use('/auth', authController);
 
 app.listen(port, () => {
-  console.log(`The express app is ready on port ${port}!`);
+  console.log(`Listening on port ${port}`);
 });
