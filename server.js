5d78b428043dd5dd3d3fd33e5f8ff03b0345bcf9
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-30 15:02:51 -0700

    Add foods controller and import in server

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
-const authController = require('./controllers/auth.js');
+const authController = require('./controllers/auth');
+const foodsController = require('./controllers/foods')
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -33,6 +34,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/auth', authController);
+app.use('/users/:userId/foods', foodsController);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
