00f6104af7cd07a928eff2b4cc16cc7ef3535a40
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-08-01 15:29:22 -0700

    Add post route to add new foods to the db

--- controllers/foods.js
+++ controllers/foods.js
@@ -19,4 +19,16 @@ router.get('/new', async (req, res) => {
     res.render('foods/new.ejs');
 });
 
+router.post('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.pantry.push(req.body);
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/foods`);
+    } catch (err) {
+        console.log(err);
+        res.redirect('/');
+    };
+});
+
 module.exports = router;
